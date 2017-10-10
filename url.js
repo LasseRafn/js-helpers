45ad42bb191595c53c2be6230458bde03a6b3609
Lasse Rafn <lasserafn@gmail.com>
2017-10-10 09:16:19 +0200

    Create url.js
    Added `http_build_query`

---
+++ url.js
@@ -0,0 +1,32 @@
+/* 
+ * Build URL query from object / array.
+ * PHP: http://php.net/manual/en/function.http-build-query.php
+ * 
+ * Usage: http_build_query({ foo: "bar", star: "wars })
+ * Output: foo=bar&star=wars
+ * 
+ * Usage: http_build_query(["foo", "bar"], "form_")
+ * Output: form_0=foo&form_1=bar
+ * 
+ * Usage: http_build_query({ foo: "bar", star: "wars }, "", "$")
+ * Output: foo=bar$star=wars
+ */
+function http_build_query(query_data, numeric_prefix, arg_separator) {
+	arg_separator = arg_separator || "&";
+	numeric_prefix = numeric_prefix || "";
+	
+	if(Array.isArray(query_data)) {
+		query_data = query_data.reduce(function(acc, cur, i) {
+			acc[numeric_prefix + i] = cur;
+			
+			return acc;
+		}, {});
+	}
+  
+  var esc = encodeURIComponent;
+  
+  return Object.keys(params)
+							 	.map(function (k) {
+							 		return esc(k) + '=' + esc(params[k])
+								}).join(arg_separator);
+}
