f6ac9e68cb11406a5f703cacf4ad88eeb1c2eb62
Lasse Rafn <lasserafn@gmail.com>
2017-10-10 09:24:42 +0200

    Update url.js

--- url.js
+++ url.js
@@ -12,21 +12,21 @@
  * Output: foo=bar$star=wars
  */
 function http_build_query(query_data, numeric_prefix, arg_separator) {
-	arg_separator = arg_separator || "&";
-	numeric_prefix = numeric_prefix || "";
-	
-	if(Array.isArray(query_data)) {
-		query_data = query_data.reduce(function(acc, cur, i) {
-			acc[numeric_prefix + i] = cur;
-			
-			return acc;
-		}, {});
-	}
+  arg_separator = arg_separator || "&";
+  numeric_prefix = numeric_prefix || "";
+
+  if(Array.isArray(query_data)) {
+    query_data = query_data.reduce(function(acc, cur, i) {
+      acc[numeric_prefix + i] = cur;
+
+      return acc;
+    }, {});
+  }
   
   var esc = encodeURIComponent;
   
   return Object.keys(params)
-  							.map(function (k) {
-							 		return esc(k) + '=' + esc(params[k])
+  		.map(function (k) {
+		  return esc(k) + '=' + esc(params[k])
                 }).join(arg_separator);
 }
